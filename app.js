36060117fcda5b406788ba46a89ee05e983e2798
nrllhkck <nurullahkucuk@yahoo.com>
2021-11-20 01:53:09 +0300

    convert to mvc

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
 const app = express();
-const Post = require('./models/Post');
+const pageControllers = require('./controllers/pageControllers')
+const postControllers = require('./controllers/postControllers')
 
 const port = 5000;
 
@@ -20,32 +21,26 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
+app.use(methodOverride('_method', {
+  methods:['POST','GET']
+}));
 
 // Routes
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index',{posts});
-});
+app.get('/', postControllers.getAllPosts);
 
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post', {
-    post
-  })
-});
+app.get('/posts/:id', postControllers.getOnePost);
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+app.get('/about', pageControllers.getAboutPage);
 
-app.get('/add', (req, res) => {
-  res.render('add');
-});
+app.get('/add', pageControllers.getAddPage);
 
-app.post('/posts', async (req, res) => { // yeni yazı eklendiğinde gelen veriyi yakalamak için post işlemi yapıyoruz.
-  await Post.create(req.body)
-  res.redirect('/')
-});
+app.post('/posts', postControllers.postCreater);
+
+app.get('/posts/edit/:id', pageControllers.getEditPage);
+
+app.put('/posts/:id', postControllers.updatePost);
+
+app.delete('/posts/:id', postControllers.deletePost);
 
 app.listen(port, () => {
   console.log('Sunucu başlatıldı...');
---
+++ controllers/postControllers.js
@@ -0,0 +1,38 @@
+const Post = require('../models/Post');
+
+exports.getAllPosts = async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts
+  });
+};
+
+exports.getOnePost = async (req, res) => {
+  const post = await Post.findById(req.params.id)
+  res.render('post', {
+    post
+  })
+};
+exports.postCreater = async (req, res) => { // yeni yazı eklendiğinde gelen veriyi yakalamak için post işlemi yapıyoruz.
+  await Post.create(req.body)
+  res.redirect('/')
+};
+exports.updatePost = async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  post.title = req.body.title
+  post.detail = req.body.detail
+  post.save()
+  res.redirect(`/posts/${req.params.id}`)
+};
+exports.deletePost = async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  await Post.findByIdAndRemove(req.params.id);
+  res.redirect('/')
+};
+
+
+
+
+
+
+
